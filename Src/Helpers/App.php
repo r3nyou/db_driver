9ad7d3471826cf0f5bd10594a088b901fca3346a
r3nyou <renyou815@gmail.com>
2020-02-05 21:38:14 +0800

    setup

---
+++ Src/Helpers/App.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Helpers;
+
+class App
+{
+    private $config = [];
+
+    public function __construct()
+    {
+        $this->config = Config::get('app');
+    }
+
+    public function isDebugMode(): bool
+    {
+        return $this->config['debug'] ?? false;
+    }
+
+    public function getEnvironment(): string
+    {
+        return $this->config['env'] ?? 'production';
+    }
+
+    public function getLogPath(): string
+    {
+        if (! isset($this->config['log_path'])) {
+            throw new \Exception('Log path is not defined');
+        }
+
+        return $this->config['log_path'];
+    }
+
+    public function isRunningFromConsole(): bool
+    {
+        return php_sapi_name() == 'cli' || php_sapi_name() == 'phpbg';
+    }
+
+    public function getServerTime(): \DateTimeInterface
+    {
+        return new \DateTime('now', new \DateTimeZone('Asia/Taipei'));
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-declare(strict_type = 1);
+// declare(strict_type = 1);
 
 require_once __DIR__.'/vendor/autoload.php';
+
+$application = new \App\Helpers\App();
+
+echo $application->isDebugMode() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->isRunningFromConsole() . PHP_EOL;
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
